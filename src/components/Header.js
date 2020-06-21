b997ed4cdbadfb3de266d6816c9032c6ffac033e
fahim-irshad <66278214+fahim-irshad@users.noreply.github.com>
2020-06-21 17:23:40 +0500

    Expense Tracker App

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+//import { Balance } from './Balance'
+
+export const Header = () => {
+    return (
+        
+        <h2 id ="balance">
+            Expense Tracker
+        </h2>
+        
+    )
+}
